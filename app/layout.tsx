6b61a0307f6760c03fc73cdfc51c2a9621556b91
Linus Jansson <linus.jansson2@uppsala.se>
2025-11-19 22:30:36 +0100

    feat: optimized fonts and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Pixelify_Sans } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/Footer";
 import { Navigation } from "@/components/Navigation";
+import { Analytics } from '@vercel/analytics/react';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+
+const pixelSans = Pixelify_Sans({
+	variable: "--font-pixelify-sans",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -27,11 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pixelSans.variable} antialiased`}
       >
 				<Navigation />
 				{children}
 				<Footer />
+				<Analytics />
       </body>
     </html>
   );
